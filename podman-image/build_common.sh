85ccd922717f24e2c48dc90ac2bc2d0722f97530
Paul Holzinger <pholzing@redhat.com>
2026-06-05 15:04:42 +0200

    update copr repo links
    Due the repo move the copr location changed as well so update it here.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>
    (cherry picked from commit 45134dd7a8fbaf4eb707fe5b4de0f4dfb3872934)
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- podman-image/build_common.sh
+++ podman-image/build_common.sh
@@ -73,8 +73,8 @@ else
     shopt -s nullglob
     FILE="/var/tmp/rpms/*.rpm"
     if [[ -n $(echo $FILE) ]]; then dnf update -y --best --allowerasing $FILE; fi
-    curl --fail -o /etc/yum.repos.d/podman-release-copr.repo https://copr.fedorainfracloud.org/coprs/packit/containers-podman-${PODMAN_PR_NUM}/repo/fedora-rawhide/packit-containers-podman-${PODMAN_PR_NUM}-fedora-rawhide.repo
-    curl --fail -o /etc/pki/rpm-gpg/podman-release-copr.gpg https://download.copr.fedorainfracloud.org/results/packit/containers-podman-${PODMAN_PR_NUM}/pubkey.gpg
+    curl --fail -o /etc/yum.repos.d/podman-release-copr.repo https://copr.fedorainfracloud.org/coprs/packit/podman-container-tools-podman-${PODMAN_PR_NUM}/repo/fedora-rawhide/packit-podman-container-tools-podman-${PODMAN_PR_NUM}-fedora-rawhide.repo
+    curl --fail -o /etc/pki/rpm-gpg/podman-release-copr.gpg https://download.copr.fedorainfracloud.org/results/packit/podman-container-tools-podman-${PODMAN_PR_NUM}/pubkey.gpg
     dnf install --best -y podman
 fi
 
