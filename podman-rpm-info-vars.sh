00fe3e6220295035270cca91d1fea49971f9834f
mheon <7735048+mheon@users.noreply.github.com>
2026-06-25 16:38:05 +0000

    Bump Podman to v5.8.4
    Signed-off-by: mheon <7735048+mheon@users.noreply.github.com>

--- podman-rpm-info-vars.sh
+++ podman-rpm-info-vars.sh
@@ -5,5 +5,5 @@
 # 2. Both PODMAN_VERSION and PODMAN_PR_NUM will have to be updated manually on release
 # PRs.
 # 3. If PODMAN_PR_NUM is empty, rpms will be fetched from the `rhcontainerbot/podman-next` copr.
-export PODMAN_VERSION="5.8.3"
-export PODMAN_PR_NUM="28916"
+export PODMAN_VERSION="5.8.4"
+export PODMAN_PR_NUM="29044"
